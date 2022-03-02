b0a5c43a7593b02d9f3a10a45ee1da75863c9c31
Tawsif Rahman Shopnil <76777358+tawsif-rahman-shopnil@users.noreply.github.com>
2022-03-02 22:20:18 +0600

    Update contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -1,7 +1,7 @@
 <?php
 // Configure your Subject Prefix and Recipient here
 $subjectPrefix = '[Contact via website]';
-$emailTo       = 'contact@your-gmail.com';
+$emailTo       = 'admin@tawsifrahmanshopnil.live';
 
 $errors = array(); // array to hold validation errors
 $data   = array(); // array to pass back data
